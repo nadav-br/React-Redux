0e5a1e091df0c773f1305f0ef8c938a10276609d
nadav-br <nadav.br@gmail.com>
2021-02-08 10:31:10 +0200

    Updates

---
+++ precache-manifest.309769418b1a8d6be7d5a71b5e73c837.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ad1e66db075dab6b8ed2151b9460ac",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "75e87a1168f91016b6f6",
+    "url": "/React-Redux/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4382d62aecd8681ff93b",
+    "url": "/React-Redux/static/js/2.c7f53038.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux/static/js/2.c7f53038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e87a1168f91016b6f6",
+    "url": "/React-Redux/static/js/main.22b07956.chunk.js"
+  },
+  {
+    "revision": "342fbf8548c1b33db129",
+    "url": "/React-Redux/static/js/runtime-main.a6db9b51.js"
+  }
+]);
